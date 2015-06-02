6c019440628a2f2934f2b3e8eb250ddd4582ebdf
michaeld <michaeld@liveperson.com>
2015-06-02 08:43:25 +0300

    deleted new line

--- build/connect.js
+++ build/connect.js
@@ -25,7 +25,6 @@ module.exports = {
                         return next();
                     }
                 });
-
                 middlewares.unshift(connect.json());
 
                 return middlewares;
