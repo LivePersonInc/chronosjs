eaa0b590788831200e68253e66b7d179140c59d7
eliranm <eliranm@liveperson.com>
2017-05-21 11:44:29 +0300

    refactor: DRY

--- build/aliases.js
+++ build/aliases.js
@@ -1,15 +1,10 @@
 module.exports = function (grunt, options) {
 
-    var tasks = ['node_version', 'jshint', 'env', 'instrument', 'connect', 'mocha', 'makeReport', 'concat', 'concat_in_order', 'uglify'];
+    var testTasks = ['env', 'instrument', 'connect', 'mocha', 'makeReport'];
+    var tasks = [].concat(['node_version', 'jshint'], testTasks, ['concat', 'concat_in_order', 'uglify']);
     return {
         'tasks': ['availabletasks'],
         'default': tasks,
-        'test': [
-            'node_version',
-            'env',
-            'instrument',
-            'connect',
-            'mocha'
-        ]
+        'test': testTasks
     };
 };
